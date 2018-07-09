36b9bb3d3d075e8b0706f803ffaaaaf5480fde1f
ballsoyed <40580907+ballsoyed@users.noreply.github.com>
2018-07-10 02:06:30 +0700

    Add files via upload

---
+++ client.php
@@ -0,0 +1,16 @@
+
+<?php
+$file = 'Do.txt.http://178.128.103.38:85/free.ovpn';
+
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+}
+?>
